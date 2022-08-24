55d485af27b6279502ffb354fb0932733cf141a1
PRATHAMESH SHUKLA <53365574+pratham-0709@users.noreply.github.com>
2022-08-24 16:02:01 +0530

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title> Test-App </title>
+    </head>
+    <body>
+        <h1>Welcome To The Cafe</h1>
+        <ul>
+            <?php
+
+                $json = ""
+                $obj = 
+
+                $products = $obj->products;
+
+                foreach ($products as $product) {
+                    echo "<li>$product</li>";
+                }
+            ?>
+        </ul>
+    </body>
+</html>
+                    
+
